e86b9469f123783a08b9de7066a31c0dd2c546ea
João Souto <joaosouto0@outlook.com>
2024-02-29 08:29:33 -0300

    Aula 2

--- aula2/variaveis.js
+++ aula2/variaveis.js
@@ -9,3 +9,15 @@ console.log("Tipo var: ",numero);
 numero = 13;
 
 console.log(texto + " o número agora é " + numero);
+
+texto = 69;
+
+console.log(texto + numero); // concatenar com "+" dois valores numericos resulta em uma soma
+
+//console.log(oQueEisso)
+
+//Constantes não podem ter novos valores assinalados a elas
+
+//oQueEisso = "uma constante????"
+
+//console.log(oQueEisso)
